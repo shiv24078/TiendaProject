c49e1893cf9101331ab3f726b42477a7c348db6e
Sfolgueras <sfolgueras@nigul.cide.es>
2023-05-05 19:07:28 +0200

    Metiendo cosas

--- static/js/home.js
+++ static/js/home.js
@@ -9,3 +9,8 @@ window.addEventListener('load', function() {
     section.style.color = '';
   });
 });
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: true
+})
